7f7ece7d0ea8ed59691c3251ab8d35b179c12ef8
Yuning Shi <shiyuning@gmail.com>
2025-03-25 16:16:17 -0400

    :sparkles: Use SoilGrids organic carbon stocks product

--- conus_cropland_soc/calculate_cropland_soc.py
+++ conus_cropland_soc/calculate_cropland_soc.py
@@ -1,5 +1,5 @@
 """This script calculates cropland areas and SOC weights for CONUS counties.
-The cropland map is based on the LGRIP30 L3 version 2 dataset, and the soil parameters are from the SoilGrids250m
+The cropland map is based on the LGRIP30 L3 version 2 dataset, and the soil organic carbon stocks are from SoilGrids250m
 version 2.0 dataset.
 """
 import geopandas as gpd
@@ -7,13 +7,12 @@ import numpy as np
 import os
 import pandas as pd
 import rioxarray
+from cycles.gadm import read_gadm
+from cycles.soilgrids import read_soilgrids_maps, reproject_match_soilgrids_maps
 from shapely.geometry import Polygon
-from config import TOTAL_DEPTH, AREA_SOC_CSV, MIN_REPORT_AREA
+from config import GADM_PATH, SOILGRIDS_PATH
+from config import AREA_SOC_CSV, MIN_REPORT_AREA
 from config import LU_MAP, LU_TYPES, AG_TYPES
-from config import WGS84
-from gadm import read_gadm
-from soilgrids import SOILGRIDS_LAYERS
-from soilgrids import read_soilgrids_maps, reproject_match_soilgrids_maps
 
 CONUS_CENTRAL_LON = -98.583 # central longitude of the CONUS (degree)
 DI = DJ = 0.00026949    # LGRI30 grid size (degree)
@@ -47,15 +46,11 @@ def calculate_grid_areas(latitudes, crs):
     return area_gdf
 
 
-def soc_weight(bulk_density, soc_percent, thickness_meter):
-    return soc_percent * 0.01 * thickness_meter * bulk_density * 1.0E4
-
-
-def calculate_cropland_soc(lu_xds, area_gdf, soilgrids_xds, boundary, county_id, layers, variables):
+def calculate_cropland_soc(lu_xds, area_gdf, soilgrids_xds, boundary, county_id, variables):
     with open(f'./temp/{county_id}', 'w') as f: pass
 
     # Align SoilGrids maps with cropland map
-    df = reproject_match_soilgrids_maps(soilgrids_xds, lu_xds, 'lu', boundary, layers, ['bulk_density', 'soc'])
+    df = reproject_match_soilgrids_maps(soilgrids_xds, lu_xds, 'lu', boundary, ['0-30cm'], ['organic_carbon_stocks'])
 
     # No cropland
     if df[df['lu'].isin(AG_TYPES)].empty : return [0.0, 0.0] + list(np.nan * np.ones(len(variables) - 2))
@@ -65,20 +60,17 @@ def calculate_cropland_soc(lu_xds, area_gdf, soilgrids_xds, boundary, county_id,
     df['ind'] = df['y'].map(lambda y: IND_J(y))
     df = pd.merge(df, area_gdf, on='ind', how='left')
 
-    for layer in layers:
-        df[f'soc_weight_{layer}'] = df.apply(lambda x: soc_weight(x[f'bulk_density_{layer}'], x[f'soc_{layer}'], SOILGRIDS_LAYERS[layer]['thickness']), axis=1)
-
-    df[f'soc_weight_0-{int(TOTAL_DEPTH * 100)}cm'] = df[[f'soc_weight_{layer}' for layer in layers]].sum(axis=1, skipna=False)
-
     result = {}
     for t in LU_TYPES:
         area = df[df['lu'].isin(LU_TYPES[t])]['area_ha'].sum()
         result[f'{t}_area'] = area if area > MIN_REPORT_AREA else 0.0
 
-        for layer in layers + [f'0-{int(TOTAL_DEPTH * 100)}cm']:
-            sub_df = df.loc[df['lu'].isin(LU_TYPES[t]), f'soc_weight_{layer}'].copy()
-            for f in FUNCS:
-                result[f'soc_{t}_{f}_{layer}'] = np.nan if sub_df.empty or result[f'{t}_area'] == 0.0 else FUNCS[f](sub_df)
+        sub_df = df.loc[df['lu'].isin(LU_TYPES[t])]
+        if sub_df.empty or result[f'{t}_area'] == 0.0:
+            result.update({f'soc_{t}_{f}': np.nan for f in FUNCS})
+            continue
+
+        result.update({f'soc_{t}_{f}': FUNCS[f](sub_df['organic_carbon_stocks_0-30cm']) for f in FUNCS})
 
     return [result[v] for v in variables]
 
@@ -106,28 +98,25 @@ def write_to_csv(conus_gdf, variables):
 
 def main():
     # Read CONUS counties
-    conus_gdf = read_gadm('USA', 'county', conus=True)
+    conus_gdf = read_gadm(GADM_PATH, 'USA', 'county', conus=True)
 
     # Read cropland map
     lu_xds = rioxarray.open_rasterio(LU_MAP, masked=True)
 
     # Calculate the areas of each LGRIP30 grid
-    area_gdf = calculate_grid_areas(lu_xds.coords['y'], WGS84)
-
-    # Find SoilGrids layers that are shallower than the total depth
-    layers = [layer for layer in SOILGRIDS_LAYERS if SOILGRIDS_LAYERS[layer]['bottom'] <= TOTAL_DEPTH]
+    area_gdf = calculate_grid_areas(lu_xds.coords['y'], lu_xds.rio.crs)
 
     os.makedirs('temp', exist_ok=True)
 
     # Generate a list of all variables that need to be calculated
     variables = [f'{t}_area' for t in LU_TYPES]
-    for layer in layers + [f'0-{int(TOTAL_DEPTH * 100)}cm']:
-        for t in LU_TYPES:
-            for v in FUNCS:
-                variables.append(f'soc_{t}_{v}_{layer}')
+    for t in LU_TYPES:
+        for v in FUNCS:
+            variables.append(f'soc_{t}_{v}')
 
     # Calculate cropland areas and SOC weights
     output_df = pd.DataFrame()
+
     for i in range(52):
         state_id = f'USA.{i}_1'
         if state_id not in conus_gdf['GID_1'].unique(): continue
@@ -135,10 +124,10 @@ def main():
         sub_gdf = conus_gdf[conus_gdf['GID_1'] == state_id].copy()
 
         # Read SoilGrids maps
-        soilgrids_xds = read_soilgrids_maps(state_id, layers, ['bulk_density', 'soc'], WGS84)
+        soilgrids_xds = read_soilgrids_maps(SOILGRIDS_PATH, state_id, ['0-30cm'], ['organic_carbon_stocks'])
 
         sub_gdf[variables] = sub_gdf.apply(
-            lambda x: calculate_cropland_soc(lu_xds, area_gdf, soilgrids_xds, x['geometry'], x['GID'], layers, variables),
+            lambda x: calculate_cropland_soc(lu_xds, area_gdf, soilgrids_xds, x['geometry'], x['GID'], variables),
             axis=1,
             result_type='expand',
         )
--- conus_cropland_soc/config.py
+++ conus_cropland_soc/config.py
@@ -1,6 +1,9 @@
-TOTAL_DEPTH = 0.3 # m
-AREA_SOC_CSV = f'./conus_cropland_soc_0-{int(TOTAL_DEPTH * 100)}cm.csv'
+AREA_SOC_CSV = f'./conus_cropland_soc_0-30cm.csv'
 MIN_REPORT_AREA = 10.0  # minimum area to report (ha)
+
+GADM_PATH = '/storage/home/yzs123/work/data/gadm/'
+
+SOILGRIDS_PATH = '/storage/home/yzs123/work/data/SoilGrids/'
 
 LU_MAP = f'/storage/home/yzs123/work/data/LGRIP30_L3_v002/LGRIP30_L3_2020_v002.tif'
 LU_TYPES = {
